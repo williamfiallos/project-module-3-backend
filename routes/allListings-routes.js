3f6553f1c626837f6674c11b67c6d616d6ac6bbc
williamfiallos <wfiallos10@gmail.com>
2019-05-06 20:47:00 -0400

    update MyListings backened

--- routes/allListings-routes.js
+++ routes/allListings-routes.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const CarPost = require("../models/car-post-model");
 const HousePost = require("../models/house-post-model");
+const User = require("../models/user-model");
 
 router.get("/all-listings", (req, res, next) => {
   const allListings = [];
@@ -17,6 +18,19 @@ router.get("/all-listings", (req, res, next) => {
           res.json(allListings);
         })
         .catch(error => next(error));
+    })
+    .catch(error => next(error));
+});
+
+router.get("/my-listings", (req, res, next) => {
+  
+
+  User.findOne(req.user._id)
+    .populate({ path: "posts.cars"})
+    .populate({ path: "posts.houses" })
+    .then( foundUser => {
+      // console.log("found User: ", foundUser.posts);
+      res.json(foundUser.posts);
     })
     .catch(error => next(error));
 });
