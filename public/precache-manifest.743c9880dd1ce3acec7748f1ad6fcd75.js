6e61e0d8e7108df7246b2e0d373d5959c9c43d09
williamfiallos <wfiallos10@gmail.com>
2019-05-08 22:14:37 -0400

    heroku setup

---
+++ public/precache-manifest.743c9880dd1ce3acec7748f1ad6fcd75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d158e5d8381b95ebaf5d076238ea03a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5cfb25d8f82753c5b14",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "f59817f95220bdd79075",
+    "url": "/static/css/main.61026fde.chunk.css"
+  },
+  {
+    "revision": "c5cfb25d8f82753c5b14",
+    "url": "/static/js/2.a3e79aa9.chunk.js"
+  },
+  {
+    "revision": "f59817f95220bdd79075",
+    "url": "/static/js/main.3deb95ae.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
